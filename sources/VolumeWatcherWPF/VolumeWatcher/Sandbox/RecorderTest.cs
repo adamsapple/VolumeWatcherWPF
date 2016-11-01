a56806dd5614b9c6e1a5b8a61594780e74eb30bd
adamsapple <hardrock.is.my.life@gmail.com>
2016-11-01 23:53:22 +0900

    デバイス情報を表示するMethodを追加。引き続き検証。 Captureデバイスのmuteやvolは取得可能だった。muteは設定もできた。volは未検証。 マイクのON/OFFはできるっぽい。

--- sources/VolumeWatcherWPF/VolumeWatcher/Sandbox/RecorderTest.cs
+++ sources/VolumeWatcherWPF/VolumeWatcher/Sandbox/RecorderTest.cs
@@ -11,31 +11,74 @@ namespace VolumeWatcher.Sandbox
 {
     class RecorderTest
     {
+
         public RecorderTest()
         {
-            // get the speakers (1st render + multimedia) device
             var deviceEnumerator = new MMDeviceEnumerator();
 
             // get default device.
-            //device = deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eCapture, ERole.eCommunications|ERole.eMultimedia);
+            var device = deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eCapture, ERole.eCommunications);
+
+            //PutDeviceInfo(device);
+            device.AudioEndpointVolume.Mute = !device.AudioEndpointVolume.Mute;
 
             var collection = deviceEnumerator.EnumAudioEndpoints(EDataFlow.eCapture, EDeviceState.Active);
 
             var devices = new List<MMDevice>();
 
-
-
             for (int i = 0, len = collection.Count; i < len; ++i)
             {
+                if (device == collection[i])
+                {
+                    Console.WriteLine("this is default;"+i);
+                }
+
                 devices.Add(collection[i]);
             }
 
             devices.ForEach((i) => {
-                var str = (string)i.GetProperty(PropertyKeys.PKEY_DEVICE_FRIENDLY_NAME);
-                //var str = device.FriendlyName;
-                Console.WriteLine(str);
+                if (device.Equals(i))
+                {
+                    Console.WriteLine("this is default;");
+                }
+                PutDeviceInfo(i);
             });
+        }
 
+        /// <summary>
+        /// MMDeviceの情報を標準出力へWriteLineする
+        /// </summary>
+        /// <param name="device"></param>
+        private void PutDeviceInfo(MMDevice device)
+        {
+            var keys = new List<PropertyKey>
+            {
+                PropertyKeys.PKEY_DEVICE_INTERFACE_FRIENDLY_NAME,
+                PropertyKeys.PKEY_AUDIO_ENDPOINT_FORM_FACTOR,
+                PropertyKeys.PKEY_AUDIO_ENDPOINT_CONTROL_PANEL_PAGE_PROVIDER,
+                PropertyKeys.PKEY_AUDIO_ENDPOINT_ASSOCIATION,
+                PropertyKeys.PKEY_AUDIO_ENDPOINT_PHYSICAL_SPEAKERS,
+                PropertyKeys.PKEY_AUDIO_ENDPOINT_GUID,
+                PropertyKeys.PKEY_AUDIO_ENDPOINT_DISABLE_SYS_FX,
+                PropertyKeys.PKEY_AUDIO_ENDPOINT_FULL_RANGE_SPEAKERS,
+                PropertyKeys.PKEY_AUDIO_ENDPOINT_SUPPORTS_EVENT_DRIVEN_MODE,
+                PropertyKeys.PKEY_AUDIO_ENDPOINT_JACK_SUB_TYPE,
+                PropertyKeys.PKEY_AUDIO_ENGINE_DEVICE_FORMAT,
+                PropertyKeys.PKEY_AUDIO_ENGINE_OEM_FORMAT,
+                PropertyKeys.PKEY_DEVICE_FRIENDLY_NAME,
+                PropertyKeys.PKEY_DEVICE_DESCRIPTION,
+                PropertyKeys.PKEY_DEVICE_ICON,
+                PropertyKeys.PKEY_SYSTEM_NAME
+            };
+
+            keys.ForEach((i) => {
+                var str = device.GetProperty(i);
+                Console.WriteLine("\t:({0}) {1}", str.GetType(), str);
+            });
+            
+            Console.WriteLine("level:"+device.AudioEndpointVolume.MasterVolumeLevelScalar);
+            Console.WriteLine("mute :"+device.AudioEndpointVolume.Mute);
+            Console.WriteLine("----");
         }
     }
 }
