a16c61a5c57eea3f1e6696e8d16c2ee636d6276c
adamsapple <hardrock.is.my.life@gmail.com>
2016-12-02 14:09:48 +0900

    モニタの電源をON/OFFするクラスを追加(未使用)

---
+++ sources/VolumeWatcherWPF/Moral/Util/MoniterPower.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Moral.Util
+{
+    public static class MoniterPower
+    {
+
+        private const int  HWND_BROADCAST   = -1;
+        private const uint WM_SYSCOMMAND    = 0x0112;
+        private const int  SC_MONITORPOWER  = 0xF170;
+        private const int  POWER_ON         = -1;
+        private const int  POWER_OFF        = 2;
+
+
+
+        public static void PowerOff()
+        {
+            NativeMethods.PostMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_OFF);
+        }
+
+        public static void PowerOn()
+        {
+            NativeMethods.PostMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_ON);
+        }
+    }
+}
---
+++ sources/VolumeWatcherWPF/Moral/Util/NativeMethods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace Moral.Util
+{
+    internal static class NativeMethods
+    {
+        // マウスイベント(mouse_eventの引数と同様のデータ)
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public int dwExtraInfo;
+        };
+
+        // キーボードイベント(keybd_eventの引数と同様のデータ)
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct KEYBDINPUT
+        {
+            public short wVk;
+            public short wScan;
+            public int   dwFlags;
+            public int   time;
+            public int   dwExtraInfo;
+        };
+
+        // ハードウェアイベント
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct HARDWAREINPUT
+        {
+            public int   uMsg;
+            public short wParamL;
+            public short wParamH;
+        };
+
+        // 各種イベント(SendInputの引数データ)
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)]
+            public MOUSEINPUT mi;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        };
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        internal static extern IntPtr SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        internal static extern IntPtr PostMessage(int hWnd, uint Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        internal static extern void SendInput(int nInputs, ref INPUT pInputs, int cbsize);
+
+    }
+}
