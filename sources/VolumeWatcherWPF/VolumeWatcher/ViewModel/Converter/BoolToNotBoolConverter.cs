de928a2c281e697f9615494f624cd97d23e7edfd
adamsapple <hardrock.is.my.life@gmail.com>
2016-12-05 18:52:53 +0900

    ToggleのStyleになんかバグがある。回避はしてあるが、調査しておいた方が良い雰囲気。

---
+++ sources/VolumeWatcherWPF/VolumeWatcher/ViewModel/Converter/BoolToNotBoolConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace VolumeWatcher.ViewModel.Converter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(bool))]
+    class BoolToNotBoolConverter : IValueConverter
+    {
+        /// <summary>
+        /// bool to !bool
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns>bool</returns>
+        public object Convert(object value, Type targetType, object parameter,
+                              System.Globalization.CultureInfo culture)
+        {
+            var result = (bool)value;
+            
+            return !result;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                        object parameter, System.Globalization.CultureInfo culture)
+        {
+            var result = (bool)value;
+
+            return !result;
+        }
+    }
+}
+
