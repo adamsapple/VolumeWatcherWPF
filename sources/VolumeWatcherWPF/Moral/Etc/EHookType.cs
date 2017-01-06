fdabf390e2dc4f719b075657f8be372449e37cdf
adamsapple <hardrock.is.my.life@gmail.com>
2017-01-06 14:53:51 +0900

    GlobalHookのライブラリを整備。これから。

---
+++ sources/VolumeWatcherWPF/Moral/Etc/EHookType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// http://hp.vector.co.jp/authors/VA016117/hook.html
+// http://mrxray.on.coocan.jp/Delphi/Others/A_KindOfHook.htm
+namespace Moral.Etc
+{
+    public enum EHookType
+    {
+        WH_CALLWNDPROC,
+        WH_CALLWNDPROCRET,
+        WH_CBT,
+        WH_DEBUG,
+        WH_FOREGROUNDIDLE,
+        WH_GETMESSAGE,
+        WH_JOURNALRECORD,
+        WH_JOURNALPLAYBACK,
+        WH_KEYBOARD_LL,
+        WH_KEYBOARD,
+        WH_MOUSE_LL,
+        WH_MOUSE,
+        WH_MSGFILTER,
+        WH_SYSMSGFILTER,
+        WH_SHELL,
+    }
+}
--- sources/VolumeWatcherWPF/VolumeWatcher/ViewModel/Converter/ReadOnlyConverter.cs
+++ sources/VolumeWatcherWPF/VolumeWatcher/ViewModel/Converter/ReadOnlyConverter.cs
@@ -4,6 +4,11 @@ using System.Windows.Data;
 
 namespace VolumeWatcher.ViewModel.Converter
 {
+
+    /// <summary>
+    /// Readonlyなコンバータ。
+    /// ConvertBackでNotImplementedExceptionを呼ぶようにしてある。ConvertだけOverrideしてくれれば良い。
+    /// </summary>
     abstract class ReadOnlyConverter : IValueConverter
     {
         public abstract object Convert(object value, Type targetType, object parameter,
