b036795803d5a305f50b08c536e7306da7e6a118
adamsapple <hardrock.is.my.life@gmail.com>
2017-01-11 09:37:12 +0900

    HookTestクラス準備

---
+++ sources/VolumeWatcherWPF/VolumeWatcher/Sandbox/ActiveWindowHookTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Moral.Etc;
+
+namespace VolumeWatcher.Sandbox
+{
+    public class ActiveWindowHookTest
+    {
+
+        ActiveWindowHook Hooker;
+
+        public ActiveWindowHookTest()
+        {
+            Hooker = new ActiveWindowHook();
+        }
+    }
+}
